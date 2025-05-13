5ff3b6d11597e46f9d88e3ade03b2250174d2921
Jv2350 <tp8375735@gmail.com>
2025-05-13 23:40:31 +0530

    Implement note creation functionality and update routing for NewNote component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import { Container, type RowProps } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
-
+import NewNote from "./components/NewNote";
+import NoteForm from "./components/NoteForm";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import {v4 as uuidV4} from "uuid"
 export type Note = {
   id: string;
 } & NoteData;
@@ -28,11 +32,27 @@ export type Tag = {
 };
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const noteWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  functoin oncCreateNote(data: NoteData){
+    setNotes(prevNotes => {
+      return [...prevNotes, {data, id:uuidV4(), tagIds: tags.map(tag => tag.id)}]
+    })
+  }
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>}></Route>
-        <Route path="/new" element={<h1>New</h1>}></Route>
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote}/>}></Route>
 
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
